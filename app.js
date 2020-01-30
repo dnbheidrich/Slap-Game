d6f13f1929c229262b67d50cfcd894646bf792fb
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-30 15:28:41 -0700

    changes

--- app.js
+++ app.js
@@ -1,36 +1,70 @@
-var health = 100;
-var hits = 0;
+var targetObj = {
+health: 100,
+hits: 0,
+item:[]
 
+}
+let itemList ={
+fireBall: {
+  name: "Fireball",
+  modifier: 2,
+	description: 'IT BURNS!'
+},
+iceBall: {
+  name: "Iceball",
+  modifier: 2,
+	description: 'IT IS FREEZING'
+},
+lightningBall:{
+  name: "Lightningball",
+  modifier: 2,
+	description: 'IT TINGLES'
+}
+
+}
 let healthElem = document.getElementById("health")
 let hitsElem = document.getElementById("hits")
+let targetStatusElem = document.getElementById("targetStatus")
 
 
 function slap(){
-  health--;
-  hits++
- 
- update()
+ targetObj.health--;
+  targetObj.hits++
+  update()
+  
 }
 
 function punch(){
-  health -=5;
-  hits++
- 
+ targetObj.health -=5;
+  targetObj.hits++
   update()
+  
 }
 
 function kick(){
-  health -= 10;
-  hits++
-  
+  targetObj.health -= 10;
+  targetObj.hits++
   update()
+ 
 }
 
 function update(){
-  if(health < 0){
-    health = 0
+ 
+  if(targetObj.health < 0){
+    targetObj.health = 0
   }
-healthElem.innerText = health
-hitsElem.innerText = hits
+healthElem.innerText = targetObj.health
+hitsElem.innerText = targetObj.hits
+
 }
 
+function giveFireBall(){
+  targetObj.item.push(itemList.fireBall)
+}
+
+
+
+function addMods(){
+let i = 0
+for()
+}
